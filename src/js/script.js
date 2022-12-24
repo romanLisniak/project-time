db0f119f67a22d0de2c78acc63ae4842560b964e
Roman <lesnyakromaa@gmail.com>
2022-12-24 17:06:36 +0100

    general file

--- src/js/script.js
+++ src/js/script.js
@@ -73,4 +73,38 @@ $(document).ready(function(){
       valideForms('#consultation-form');
       valideForms('#consultation form');
       valideForms('#order form');
+      $('form').submit(function(e) {
+        e.preventDefault();
+        if(!$(this).valid()){
+          return; 
+        }
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+      
+            $('form').trigger('reset');
+        });
+        return false;
+      });
+      
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 1600) {
+          $('.pageup').fadeIn();
+      } else {
+          $('.pageup').fadeOut();
+      }
+  });
+
+  $("a[href^='#']").click(function(){
+      const _href = $(this).attr("href");
+      $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+      return false;
+  });
 });
+
+
